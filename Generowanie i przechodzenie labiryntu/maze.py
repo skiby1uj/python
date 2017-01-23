ff62ddda408af414a8bc823e34a745340a36c2ef
grzegorz <skiby1@wp.pl>
2017-01-23 23:35:58 +0100

    dodanie ramki przy wyswietlaniu labiryntu oraz random mety

--- Generowanie i przechodzenie labiryntu/maze.py
+++ Generowanie i przechodzenie labiryntu/maze.py
@@ -3,10 +3,9 @@ from stack import Stack
 from RandomQueue import RandomQueue
 
 class Maze:
-    def __init__(self, x, y):
+    def __init__(self, x):
         self.x = x
-        self.y = y
-        self.value = [['#' for r in range(x)] for w in range(y)]
+        self.value = [['#' for r in range(x)] for w in range(x)]
 
     # def __getitem__(self, x, y):
     #     return self.value[x][y]
@@ -15,7 +14,7 @@ class Maze:
         randomQueue = RandomQueue()
         countShouldNeugh = None
         # print 'checkNeigh'
-        if x > 0 and y > 0 and x < self.x-1 and y < self.y-1:
+        if x > 0 and y > 0 and x < self.x-1 and y < self.x-1:
             countShouldNeugh = 4
             if str(self.value[x][y+1]) == '#':
                 randomQueue.insert([x, y+1])
@@ -25,7 +24,7 @@ class Maze:
                 randomQueue.insert([x-1, y])
             if str(self.value[x+1][y]) == '#':
                 randomQueue.insert([x+1, y])
-        if x == 0 and y > 0 and y < self.y-1:
+        if x == 0 and y > 0 and y < self.x-1:
             countShouldNeugh = 3
             if str(self.value[x][y-1]) == '#':
                 randomQueue.insert([x, y-1])
@@ -41,15 +40,15 @@ class Maze:
                 randomQueue.insert([x+1, y])
             if str(self.value[x][y+1]) == '#':
                 randomQueue.insert([x, y+1])
-        if x > 0 and y == self.y-1 and x < self.x-1:
+        if x > 0 and y == self.x-1 and x < self.x-1:
             countShouldNeugh = 3
-            if str(self.value[x+1][self.y-1]) == '#':
-                randomQueue.insert([x+1, self.y-1])
-            if str(self.value[x-1][self.y-1]) == "#":
-                randomQueue.insert([x-1, self.y-1])
+            if str(self.value[x+1][self.x-1]) == '#':
+                randomQueue.insert([x+1, self.x-1])
+            if str(self.value[x-1][self.x-1]) == "#":
+                randomQueue.insert([x-1, self.x-1])
             if str(self.value[x][y-1]) == "#":
                 randomQueue.insert([x, y-1])
-        if x == self.x-1 and y > 0 and y < self.y-1:
+        if x == self.x-1 and y > 0 and y < self.x-1:
             countShouldNeugh = 3
             if str(self.value[self.x-1][y+1]) == "#":
                 randomQueue.insert([self.x-1, y+1])
@@ -63,7 +62,7 @@ class Maze:
                 randomQueue.insert([x+1, y])
             if str(self.value[x][y+1]) == '#':
                 randomQueue.insert([x, y+1])
-        if x == 0 and y == self.y-1:
+        if x == 0 and y == self.x-1:
             countShouldNeugh = 2
             if str(self.value[x][y-1]) == '#':
                 randomQueue.insert([x, y-1])
@@ -75,7 +74,7 @@ class Maze:
                 randomQueue.insert([x, y+1])
             if str(self.value[x-1][y]) == '#':
                 randomQueue.insert([x-1, y])
-        if x == self.x-1 and y == self.y-1:
+        if x == self.x-1 and y == self.x-1:
             countShouldNeugh = 2
             if str(self.value[x][y-1]) == '#':
                 randomQueue.insert([x, y-1])
@@ -89,16 +88,25 @@ class Maze:
         return randomQueue.count(), randomQueue, countShouldNeugh
 
     def printLab(self):
+        for i in range(0,self.x+2):
+            sys.stdout.write('*')
+        print
         for i in range(0, self.x):
-            for j in range(0, self.y):
+            sys.stdout.write('*')
+            for j in range(0, self.x):
                 sys.stdout.write(self.value[i][j])
-            print
+            print '*'
+        for i in range(0,self.x+2):
+            sys.stdout.write('*')
+        print
 
     def generate(self):
         # print "in generate:"
-        stack = Stack((self.x)*(self.y))
+        odl = 0
+        sett = RandomQueue((self.x)*(self.x))
+        stack = Stack((self.x)*(self.x))
         count, params, countShouldNeigh = self.checkNeigh(0,0)
-        self.value[0][0] = ' '
+        self.value[0][0] = 'S'
         stack.push(params)
 
         while not stack.is_empty():
@@ -112,8 +120,17 @@ class Maze:
             if params is not None and count+1 == countShouldNeigh:
                 stack.push(params)
                 self.value[randomNeighParam[0]][randomNeighParam[1]] = ' '
+                odl += 1#odl od poczatku lab
+                if odl > self.x-1:
+                    sett.insert(randomNeighParam)#dodajemy do randomQueue
+            else:
+                odl -= 1
+        # self.printLab()
+        tmp = sett.remove()
+        self.value[tmp[0]][tmp[1]] = 'M'
+        # print str(tmp)
 
-tmp = Maze(100,100)
+tmp = Maze(10)
 # tmp.printLab()
 tmp.generate()
 tmp.printLab()
